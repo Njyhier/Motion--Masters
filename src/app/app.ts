83a72616630b43664ea934c7f15776988d49e0a4
Njyhier <njerinjihia43@gmail.com>
2026-01-07 23:59:47 +0300

    added sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page';
 import { HeaderComponent } from './components/header/header-component/header-component';
+import { SidebarComponent } from './components/sidebar/sidebar-component/sidebar-component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomePageComponent, HeaderComponent],
+  imports: [HomePageComponent, HeaderComponent,SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
